3145e1133be369e19839195fde7d60f8f2b11983
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-09-24 22:22:45 +0400

    added Footer

--- src/Components/Advanced/Footer/Footer.tsx
+++ src/Components/Advanced/Footer/Footer.tsx
@@ -1,6 +1,121 @@
+// Libraries
+import Image from "next/image";
+
+// Components
+import Wrapper from "@/Components/UIKit/Wrapper/Wrapper";
+
+// Logo
+import altLogo from "@/Assets/logos/altLogo.png";
+
 // Styles
 import Style from "./Footer.module.css";
+import Link from "next/link";
 
-export default function () {
-  return <div>Footer</div>;
+interface FooterData {
+  title: string;
+  links: {
+    linkTitle: string;
+    href: string;
+  }[];
 }
+
+const data: FooterData[] = [
+  {
+    title: "Landings",
+    links: [
+      {
+        linkTitle: "Home",
+        href: "/",
+      },
+      {
+        linkTitle: "Products",
+        href: "/products",
+      },
+      {
+        linkTitle: "Services",
+        href: "/services",
+      },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      {
+        linkTitle: "Home",
+        href: "/",
+      },
+      {
+        linkTitle: "Products",
+        href: "/products",
+      },
+      {
+        linkTitle: "Services",
+        href: "/services",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      {
+        linkTitle: "Home",
+        href: "/",
+      },
+      {
+        linkTitle: "Products",
+        href: "/products",
+      },
+      {
+        linkTitle: "Services",
+        href: "/services",
+      },
+    ],
+  },
+];
+
+interface LinksWrapperProps {
+  title: string;
+  links: {
+    linkTitle: string;
+    href: string;
+  }[];
+}
+
+const Footer: React.FC = () => {
+  return (
+    <Wrapper>
+      <div className={Style.footerGrid}>
+        <div className={Style.logoWraper}>
+          <Image src={altLogo} alt="" />
+          <h6>
+            Social media validation business model canvas graphical user
+            interface launch party creative facebook iPad twitter.
+          </h6>
+        </div>
+        {data.map((item, index) => {
+          return (
+            <LinksWrapper key={index} title={item.title} links={item.links} />
+          );
+        })}
+      </div>
+      <h6 className={Style.rights}>All rights reserved.</h6>
+    </Wrapper>
+  );
+};
+
+const LinksWrapper: React.FC<LinksWrapperProps> = ({ title, links }) => {
+  return (
+    <div className={Style.linksWrapper}>
+      <h4>{title}</h4>
+      {links.map((link, index) => {
+        return (
+          <Link key={index} href={link.href}>
+            {link.linkTitle}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Footer;
