f5d8a454f47b871dc3ac2e55c978f5e3e9cca888
takudzwa-mushai <takudzwa.mushai@vaimanagement.co>
2023-09-27 09:11:56 +0400

    init services

---
+++ src/app/home/Components/services/Services.tsx
@@ -0,0 +1,8 @@
+// Styles
+import Style from "./Services.module.css";
+
+const Services: React.FC = () => {
+  return <div>Services</div>;
+};
+
+export default Services;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,12 +3,16 @@ import React from "react";
 // Components
 import HeroSectioin from "./Components/HeroSection/HeroSection";
 import Blogs from "./Components/blogs/Blogs";
+import Services from "./Components/services/Services";
 
 const Home: React.FC = () => {
   return (
     <>
       <section>
         <HeroSectioin />
+      </section>
+      <section>
+        <Services />
       </section>
       <section>
         <Blogs />
