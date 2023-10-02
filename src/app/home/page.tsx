04892c373397728dfb3da6371aad7dbb5631cef9
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-10-02 14:58:23 +0400

    init accordion

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,6 +5,7 @@ import HeroSectioin from "./Components/HeroSection/HeroSection";
 import Blogs from "./Components/blogs/Blogs";
 import Services from "./Components/services/Services";
 import CustomRow from "@/Components/UIKit/customRow/CustomRow";
+import Accordion from "./Components/accordion/Accordion";
 
 const Home: React.FC = () => {
   return (
@@ -20,6 +21,9 @@ const Home: React.FC = () => {
       </section>
       <section>
         <CustomRow version="right" />
+      </section>
+      <section>
+        <Accordion />
       </section>
       <section>
         <Blogs />
