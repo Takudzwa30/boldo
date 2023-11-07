e8571e62738a7ce946257c8adbdb2117c97559fd
takudzwa-mushai <takudzwa.mushai@vaimanagement.co>
2023-11-07 11:36:50 +0400

    update package json

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,19 +9,19 @@ import Footer from "@/Components/Advanced/Footer/Footer";
 import ContactUs from "@/Components/UIKit/contactUs/ContactUs";
 
 export default function App({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
-      <body>
-        <ThemeProvider theme={theme}>
-          {/* <Header /> */}
-          {children}
-          <ContactUs />
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+   return (
+      <html lang="en">
+         <head>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+         </head>
+         <body>
+            <ThemeProvider theme={theme}>
+               {/* <Header /> */}
+               {children}
+               <ContactUs />
+               <Footer />
+            </ThemeProvider>
+         </body>
+      </html>
+   );
 }
